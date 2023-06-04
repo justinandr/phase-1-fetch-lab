adc2974d030d0e3e51f9ecfcce65120fb07f57dc
Justin Rodriguez <justin.r@me.com>
2023-06-04 14:17:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function (resp){
+    return resp.json()
+  })
+  .then(function (data){
+    const json = data
+    renderBooks(json)
+  })
   
 }
 
